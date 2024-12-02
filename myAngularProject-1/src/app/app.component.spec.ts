ed39f46c2f16d7c24ea11081dc0c814b84178774
nnicha123 <nicha_ngamtweerat@hotmail.com>
2024-12-02 11:35:36 +0700

    Basic angular review

--- myAngularProject-1/src/app/app.component.spec.ts
+++ myAngularProject-1/src/app/app.component.spec.ts
@@ -5,12 +5,8 @@ import { AppComponent } from './app.component';
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [
-        RouterModule.forRoot([])
-      ],
-      declarations: [
-        AppComponent
-      ],
+      imports: [RouterModule.forRoot([])],
+      declarations: [AppComponent],
     }).compileComponents();
   });
 
@@ -20,16 +16,12 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'myAngularProject-1'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('myAngularProject-1');
-  });
-
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, myAngularProject-1');
+    expect(compiled.querySelector('h1')?.textContent).toContain(
+      'Hello, myAngularProject-1'
+    );
   });
 });
--- myAngularProject-1/src/app/app.component.ts
+++ myAngularProject-1/src/app/app.component.ts
@@ -3,8 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'myAngularProject-1';
+  inputVal: string = '';
+  dynamicVal: string = '';
+  count: number = 0;
+  counter(type: string) {
+    type === 'inc' ? this.count++ : this.count--;
+  }
 }
